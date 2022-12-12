4dba84c72a7d8ed15c0e2652c706319a893b0e92
nikul1ch <nikulin@netkam.ru>
2022-12-12 14:59:30 +0300

    create project

---
+++ seminar_1/src/main/java/ru/netkam/App.java
@@ -0,0 +1,13 @@
+package ru.netkam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
